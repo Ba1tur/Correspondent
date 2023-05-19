65c8245c354967c7c11702aca771a123c5258d9d
aziz7077 <azizchik261@gmail.com>
2023-05-19 15:32:01 +0600

    ok

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Footer.module.scss'
 import { motion } from 'framer-motion';
 
 const Footer = () => {
+	const [email, setEmail] = useState('');
+	const [errors, setErrors] = useState({});
+
+	const validateEmail = () => {
+		const errors = {};
+		if (!email.trim()) {
+			errors.email = 'Поле обязательно для заполнения';
+		} else if (!/\S+@\S+\.\S+/.test(email)) {
+			errors.email = 'Неправильный формат почты';
+		}
+		return errors;
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const validationErrors = validateEmail();
+		if (Object.keys(validationErrors).length === 0) {
+			// Обработка отправки данных формы
+			setEmail('');
+			setErrors({});
+		} else {
+			setErrors(validationErrors);
+		}
+	};
 
 	return (
 		<div className={s.container}>
@@ -26,6 +50,7 @@ const Footer = () => {
 					}}
 				>Больше 90% учеников прошли полный курс и смогли <br /> собрать свой первый компьютер</motion.p>
 				<motion.form
+					onSubmit={handleSubmit}
 					initial="hidden"
 					transition={{ duration: 0.7 }}
 					whileInView="visible"
@@ -34,9 +59,21 @@ const Footer = () => {
 						visible: { scale: 1 },
 					}}
 				>
-					<input type="text" placeholder='E-mail' />
-					<button>Подписаться</button>
+					<input
+						type="text"
+						placeholder='E-mail'
+						value={email}
+						onChange={(event) => setEmail(event.target.value)}
+					/>
+
+					<button
+						type="submit"
+						onClick={handleSubmit}
+					>
+						Подписаться
+					</button>
 				</motion.form>
+				{errors.email && <p className={s.errors}>{errors.email}</p>}
 				<div className={s.soc_icons}>
 					<motion.div
 						initial="hidden"
